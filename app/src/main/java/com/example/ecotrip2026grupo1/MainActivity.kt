209f66a5ee5b5f99f0151e86fc37a2ad8b834360
DaniellaPR <pauladanielapozo@gmail.com>
2026-05-21 20:24:06 -0500

    Creación de pantalla de resumen y conexión final.

--- app/src/main/java/com/example/ecotrip2026grupo1/MainActivity.kt
+++ app/src/main/java/com/example/ecotrip2026grupo1/MainActivity.kt
@@ -9,10 +9,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.example.ecotrip2026grupo1.data.PreferenciasRepositorio
+import com.example.ecotrip2026grupo1.data.dataStore
+import com.example.ecotrip2026grupo1.ui.model.EcoTripViewModel
+import com.example.ecotrip2026grupo1.ui.FormularioScreen
+import com.example.ecotrip2026grupo1.ui.ResumenScreen
 import com.example.ecotrip2026grupo1.ui.theme.EcoTrip2026Grupo1Theme
 
 class MainActivity : ComponentActivity() {
@@ -35,18 +42,41 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun EcoTripApp() {
     val navController = rememberNavController()
+    val contexto = LocalContext.current
+
+    // Instanciamos el Repositorio y el ViewModel utilizando el Factory que creamos
+    val repositorio = PreferenciasRepositorio(contexto.dataStore)
+    val viewModel: EcoTripViewModel = viewModel(
+        factory = EcoTripViewModel.provideFactory(repositorio)
+    )
 
     NavHost(
         navController = navController,
         startDestination = FormularioRuta
     ) {
         composable<FormularioRuta> {
-            // FormularioScreen(...)
+            FormularioScreen(
+                viewModel = viewModel,
+                onNavegarResumen = { resumenDatos ->
+                    // Navegación tipada hacia el resumen
+                    navController.navigate(resumenDatos)
+                }
+            )
         }
 
         composable<ResumenRuta> { backStackEntry ->
             val resumenDatos = backStackEntry.toRoute<ResumenRuta>()
-            // ResumenScreen(datos = resumenDatos, ...)
+
+            ResumenScreen(
+                datos = resumenDatos,
+                onVolverInicio = {
+                    // Requisito: Limpiar la pila de pantallas usando popUpTo y launchSingleTop
+                    navController.navigate(FormularioRuta) {
+                        popUpTo(FormularioRuta) { inclusive = true }
+                        launchSingleTop = true
+                    }
+                }
+            )
         }
     }
 }
